dee115f2881943af06f002441263c9fcdd9b7309
BHyeonKim <rlaqhguse@gmail.com>
2023-02-09 20:48:16 +0900

    refactor(ui-textinputwithbutton): 컴포넌트의 인터페이스 변경
    textInputProps를 extends하여 내부의 TextInput컴포넌트로 전달
    
    JIP-41

--- packages/ui/src/input/textInputWithButton/index.tsx
+++ packages/ui/src/input/textInputWithButton/index.tsx
@@ -1,4 +1,4 @@
-import { TextInput, View } from 'react-native-web'
+import { TextInput, TextInputProps, View } from 'react-native-web'
 
 import { Button } from '../../button'
 import COLORS from '../../constants/color'
@@ -6,24 +6,22 @@ import Title from '../title'
 
 import { styles } from './styles'
 
-interface TextInputWithButtonProps {
+interface TextInputWithButtonProps extends TextInputProps {
   isRequired?: boolean
-  placeholder: string
   title?: string
 }
 
 export function TextInputWithButton({
   isRequired,
-  placeholder,
   title,
+  ...props
 }: TextInputWithButtonProps) {
   return (
     <View style={styles.outerContainer}>
       {!!title && <Title isRequired={!!isRequired} text={title} />}
       <View style={styles.innerContainer}>
         <TextInput
-          autoComplete="email"
-          placeholder={placeholder}
+          {...props}
           placeholderTextColor={COLORS.GREY4}
           style={styles.input}
         />

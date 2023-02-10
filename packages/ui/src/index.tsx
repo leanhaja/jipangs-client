1e3846cae18f135e35e41293281316e5dd129fe3
BHyeonKim <rlaqhguse@gmail.com>
2023-02-10 16:35:19 +0900

    feat(ui-selectbutton): 2개중에 하나를 선택하는 컴포넌트
    JIP-41

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,4 @@
 export { Button } from './button'
 export { GeneralTextInput } from './input/generalTextInput'
+export { SelectButton } from './input/selectButton'
 export { TextInputWithButton } from './input/textInputWithButton'

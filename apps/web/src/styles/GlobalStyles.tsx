ab59b40facc57df2d519c5ce7fac160c08430ba1
BHyeonKim <rlaqhguse@gmail.com>
2023-02-13 02:47:57 +0900

    chore(theme): 폰트파일 추가 및 정의
    JIP-78

--- apps/web/src/styles/GlobalStyles.tsx
+++ apps/web/src/styles/GlobalStyles.tsx
@@ -7,12 +7,58 @@ export default function GlobalStyles() {
       styles={css`
         ${emotionReset}
         body {
-          font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue',
-            'Helvetica', 'Arial', sans-serif;
+          font-family: 'Pretendard', 'SF Pro Text', 'SF Pro Icons',
+            'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
           display: flex;
           height: 100vh;
           align-items: center;
           justify-content: center;
+        }
+
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-Thin.woff');
+          font-weight: 100;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-ExtraLight.woff');
+          font-weight: 200;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-Light.woff');
+          font-weight: 300;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-Regular.woff');
+          font-weight: 400;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-Medium.woff');
+          font-weight: 500;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-SemiBold.woff');
+          font-weight: 600;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-Bold.woff');
+          font-weight: 700;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-ExtraBold.woff');
+          font-weight: 800;
+        }
+        @font-face {
+          font-family: 'Pretendard';
+          src: url('/fonts/Pretendard-Black.woff');
+          font-weight: 900;
         }
       `}
     />

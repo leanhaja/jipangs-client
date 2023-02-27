185bd243164c8dc5ae9e43a5b4c06519f04f724a
BHyeonKim <rlaqhguse@gmail.com>
2023-02-27 23:56:45 +0900

    feat(web): 회원가입시 사용되는 헤더와 프로그레스 바
    JIP-108

---
+++ apps/web/src/features/register/components/progress-header/index.tsx
@@ -0,0 +1,32 @@
+import * as Styled from './styled'
+
+import IconButton from '@/components/icon-button'
+
+interface ProgressHeaderProps {
+  currentStep: number
+  onClickBack?: () => void
+  title: string
+  totalStep: number
+}
+
+export default function ProgressHeader({
+  currentStep,
+  onClickBack,
+  title,
+  totalStep,
+}: ProgressHeaderProps) {
+  return (
+    <Styled.ProgressHeader>
+      <Styled.Header>
+        <Styled.Title>{title}</Styled.Title>
+        <IconButton
+          ariaLabel="before"
+          iconName="before_arrow"
+          onClick={onClickBack}
+          style={{ left: 0, position: 'absolute' }}
+        />
+      </Styled.Header>
+      <Styled.ProgressBar currentStep={currentStep} totalStep={totalStep} />
+    </Styled.ProgressHeader>
+  )
+}
